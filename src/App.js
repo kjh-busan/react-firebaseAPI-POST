82d69ecf871b118398ef3d1727c79f8c0404f269
kjh-busan <picsmash7@gmail.com>
2023-03-29 03:03:48 +0900

    feat: add POST method with FireBase

--- src/App.js
+++ src/App.js
@@ -13,22 +13,27 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch(
+        "https://react-http-3d9ed-default-rtdb.asia-southeast1.firebasedatabase.app/movie.json/"
+      );
       if (!response.ok) {
-        throw new Error('Something went wrong!');
+        throw new Error("Something went wrong!");
       }
 
       const data = await response.json();
+      console.log(data);
+      const loadedMovies = []; // 왜 const이지?
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -39,8 +44,20 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
-    console.log(movie);
+  async function addMovieHandler(movie) {
+    const response = await fetch(
+      "https://react-http-3d9ed-default-rtdb.asia-southeast1.firebasedatabase.app/movie.json/",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        header: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const data = response.json();
+    console.log(data);
   }
 
   let content = <p>Found no movies.</p>;
